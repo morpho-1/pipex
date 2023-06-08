13927778b60fc24a786ec73219b7ca46e33ccc47
Morpho <115429757+morpho-1337@users.noreply.github.com>
2023-06-08 10:37:52 +0100

    PIPEX_H

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: aylemrab <aylemrab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 06:31:04 by aylemrab          #+#    #+#             */
-/*   Updated: 2023/06/03 02:07:24 by aylemrab         ###   ########.fr       */
+/*   Updated: 2023/06/08 10:37:25 by aylemrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,24 @@
 # include <string.h>
 # include <stdio.h>
 # include <limits.h>
+# include <fcntl.h>
 # include <signal.h>
 # include <stddef.h>
 # include <ctype.h>
-# include <stdbool.h>
-# include "libft/libft.h"
+# include "libft_tools/libft.h"
 
 typedef struct s_vars
 {
-	int		fd[2];
-	int		infile;
-	int		outfile;
-	int		pid1;
-	int		pid;
+	int			fd[2];
+	int			infile;
+	int			outfile;
+	int			pid1;
+	int			pid;
 	char		*c_path;
-	char		**env;
 }	t_vars;
 
 char	**getpath(char **env);
-int	check_access(char *join3);
+int		check_access(char *join3);
 char	*get_command_path(char *cmd, char **env_paths);
 
 #endif
